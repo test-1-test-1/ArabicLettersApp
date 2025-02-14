21182875d7df149165e6d047eefd0a5ccc802db8
test-1-test-1 <84787402+test-1-test-1@users.noreply.github.com>
2025-02-14 16:04:58 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,14 @@
-const CACHE_NAME = 'my-letter-app-cache-v1';
+const CACHE_NAME = 'arabic-letter-app-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/images-192.png',
-  '/images-512.png',
-  '/audios/ba.mp3',
-  '/audios/bo.mp3',
-  '/audios/bi.mp3',
-  '/audios/ab.mp3',
-  
-  // add more assets like images, audio files, etc.
+  '/ArabicLettersApp/',
+  '/ArabicLettersApp/index.html',
+  '/ArabicLettersApp/manifest.json',
+  '/ArabicLettersApp/images/icon-192.png',
+  '/ArabicLettersApp/images/icon-512.png',
+  '/ArabicLettersApp/audios/ba.mp3',
+  '/ArabicLettersApp/audios/bo.mp3',
+  '/ArabicLettersApp/audios/bi.mp3',
+  '/ArabicLettersApp/audios/ab.mp3'
 ];
 
 self.addEventListener('install', event => {
